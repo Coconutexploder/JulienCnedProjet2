b5f8714d55cc948d7a99d2eccbd3de0cfb8f0cbc
Coconutexploder <39191676+Coconutexploder@users.noreply.github.com>
2025-06-03 11:30:15 +0200

    Update main.cpp v2
    creation de boucle de vérification de valeur bouton
    dépendant de la valeur de bouton, led s'allume

--- LedSuitBouton/main.cpp
+++ LedSuitBouton/main.cpp
@@ -2,3 +2,11 @@
 
 DigitalOut led(PA_5);     // LED verte
 DigitalIn bouton(PC_13);  // Bouton utilisateur
+
+int main() 
+{
+    while (true) 
+    {
+        led = bouton.read(); // La LED copie l’état du bouton (0 ou 1)    //lit la valeur de bouton, change sa valeur, valeur devient 1 ou 0, led s'allume ou s'éteind
+    }
+}
